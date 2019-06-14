b935e60d3c246be3b21c83aab0a47c4f26dbf4c3
BastianBueeler <bastian.bueeler@gibmit.ch>
2019-06-14 11:25:20 +0200

    Kundenverwaltungsprogramm inklusiv Weboberfläch mit Maven

---
+++ m154-crm_maven/src/main/java/HansJoergAG/m154_crm_maven/App.java
@@ -0,0 +1,13 @@
+package HansJoergAG.m154_crm_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
